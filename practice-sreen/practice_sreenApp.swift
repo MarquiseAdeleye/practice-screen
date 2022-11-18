253cb27b4372bf7ebfdb656f12fdaccf9c0b6667
MarquiseAdeleye <marquiseadeleye@gmail.com>
2022-11-18 09:41:02 -0500

    upload-code

---
+++ practice-sreen/practice_sreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_sreenApp.swift
+//  practice-sreen
+//
+//  Created by Marquise Adeleye on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct practice_sreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
